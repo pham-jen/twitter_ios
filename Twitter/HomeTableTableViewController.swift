7f7ed9aa959ec755f3e3c2d6c0b8cc167b715bd4
jpham35 <49501108+jpham35@users.noreply.github.com>
2020-10-10 01:31:00 -0500

    added refresh control to pull and get new tweets

--- Twitter/HomeTableTableViewController.swift
+++ Twitter/HomeTableTableViewController.swift
@@ -12,13 +12,18 @@ class HomeTableTableViewController: UITableViewController {
 
     var tweetArray = [NSDictionary]()
     var numberOfTweets : Int!
+    let myRefreshControl = UIRefreshControl()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         loadTweet()
+        
+        myRefreshControl.addTarget(self, action: #selector(loadTweet), for: .valueChanged)
+        
+        tableView.refreshControl = myRefreshControl
     }
     
-    func loadTweet(){
+    @objc func loadTweet(){
         
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         
@@ -33,6 +38,7 @@ class HomeTableTableViewController: UITableViewController {
             }
             
             self.tableView.reloadData()
+            self.myRefreshControl.endRefreshing()
             
         }, failure: { (Error) in
             print("Could not receive tweets!")
